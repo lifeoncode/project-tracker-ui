c7ab12055bfcd6b1f4677a99527e7860bb5aa52d
lifeoncode <ndu918@outlook.com>
2020-05-12 22:52:03 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+
+const [menu, open, close] = [document.querySelector('#menu'), document.querySelector('#menu-btn'), document.querySelector('#close-btn')];
+
+window.addEventListener('DOMContentLoaded', init);
+
+function init(e){
+
+
+    open.addEventListener('click', openMenu);
+    close.addEventListener('click', closeMenu);
+    
+
+    function openMenu(){
+        this.classList.add('hide');
+        close.classList.remove('hide');
+
+        menu.classList.remove('hide');
+        menu.classList.add('show-menu');
+
+        setTimeout(() => {
+            menu.style.display = 'block';
+            menu.classList.remove('show-menu');
+        }, 500);
+    }
+
+    function closeMenu(){
+        this.classList.add('hide');
+        open.classList.remove('hide');
+
+        menu.classList.add('hide-menu');
+        setTimeout(() => {
+            menu.classList.add('hide');
+            menu.classList.remove('hide-menu');
+        }, 500);
+    }
+    
+    
+    
+}
